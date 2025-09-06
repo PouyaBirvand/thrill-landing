3ab9eb65534cc34bdbde0addde645d8858fe909a
PouyaBirvand <pooyabirvand@gmail.com>
2025-09-06 23:24:49 +0330

    added last changes 1404/06/15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,28 @@
-import Container from "@/components/common/Container";
-import Header from "@/components/header/Header";
-import CommissionPlanSection from "@/components/sections/commission/CommissionPlanSection";
-import FeatureSlider from "@/components/sections/features/FeatureSlider";
-import FeaturesSection from "@/components/sections/features/FeaturesSection";
-import HeroSection from "@/components/sections/hero/HeroSection";
-import AffiliateSignupForm from "@/components/sections/signup/SignUpSection";
-import StepsSection from "@/components/sections/steps/StepsSection";
-import CursorSpotlight from "@/components/ui/CursorSpotlightDots";
+import Container from "@/components/common/Container"
+import Header from "@/components/header/Header"
+import CommissionPlanSection from "@/components/sections/commission/CommissionPlanSection"
+import FeatureSlider from "@/components/sections/features/FeatureSlider"
+import FeaturesSection from "@/components/sections/features/FeaturesSection"
+import HeroSection from "@/components/sections/hero/HeroSection"
+import AffiliateSignupForm from "@/components/sections/signup/SignUpSection"
+import StepsSection from "@/components/sections/steps/StepsSection"
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen overflow-x-hidden">
-      <div className="fixed inset-0 z-0 hidden md:block">
-        <CursorSpotlight />
-      </div>
+    <div className="overflow-x-hidden">
+      <Container>
+        <Header />
+      </Container>
 
-      <div className="relative z-10">
-        <Container>
-          <Header />
-        </Container>
+      <HeroSection />
 
-        <HeroSection />
-
-        <Container className="relative z-10">          
-          <FeaturesSection />
-          <FeatureSlider />
-          <StepsSection />
-          <AffiliateSignupForm />
-          <CommissionPlanSection />
-        </Container>
-      </div>
+      <Container>
+        <FeaturesSection />
+        <FeatureSlider />
+        <StepsSection />
+        <AffiliateSignupForm />
+        <CommissionPlanSection />
+      </Container>
     </div>
-  );
+  )
 }
