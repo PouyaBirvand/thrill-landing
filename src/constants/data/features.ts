c36310ce3866c3f99b9b82345f4e82db4aa938a4
PouyaBirvand <pooyabirvand@gmail.com>
2025-09-08 13:10:08 +0330

    sort features.ts

--- src/constants/data/features.ts
+++ src/constants/data/features.ts
@@ -10,7 +10,6 @@ export const features = [
       "We’ve run affiliate programs. We know what you need – real payouts, fast tracking, and support that actually helps. That’s why everything at Thrill is built to boost your earnings and cut the noise. From custom CPA deals to weekly payments and dedicated reports – we’ve got your back.",
     more_detail:
       "At Thrill, you’re not just another signup - you’re a partner.",
-    // svgPath: "/feature/feature1.svg",
     svgComponent: Feature1SvgComponent,
     imagePath: "/feature/feature1.png",
     stats: {
@@ -28,9 +27,8 @@ export const features = [
     title: "Custom Bonuses",
     description:
       "Tailored for performance — personalized offers from Thrill boost conversions, increase player retention, and drive stronger ROI for both affiliates and players. From high rakeback to time-based rewards and VIP perks, every bonus is aligned with player behavior to keep engagement at its peak.",
-    // svgPath: "/feature/feature2.svg",
-    svgComponent: Feature2SvgComponent, 
-    imagePath: "/feature/feature2.png",
+    svgComponent: Feature5SvgComponent, 
+    imagePath: "/feature/feature5.png",
     stats: {
       primary: {
         value: "Up to 70%",
@@ -46,9 +44,7 @@ export const features = [
     title: "Creative Support",
     description:
       "Premier on-demand design tailored for campaigns — from attention-grabbing banners and immersive landing pages to targeted promos kits. Everything is crafted for conversion and brand alignment.",
-    // svgPath: "/feature/feature3.svg",
-    svgComponent: Feature3SvgComponent ,
-    imagePath: "/feature/feature3.png",
+    imagePath: "/feature/feature4.png",
     stats: {
       primary: {
         value: "24/7",
@@ -64,9 +60,8 @@ export const features = [
     title: "Fastest Payouts",
     description:
       "When you’re running traffic, every second counts. Thrill makes sure your earnings hit your account fast, so you can reinvest and scale without delays. From flexible deals to real-time tracking, we’re built to keep your momentum going. No waiting for end-of-month cycles — you get paid quickly, reliably, and with full transparency.",
-    // svgPath: "/feature/feature4.svg",
-    // svgComponent: Feature1SvgComponent , // If no specific SVG, you can set to null or a default component
-    imagePath: "/feature/feature4.png",
+      svgComponent: Feature2SvgComponent ,
+    imagePath: "/feature/feature2.png",
     stats: {
       primary: {
         value: "Referrals",
@@ -82,9 +77,8 @@ export const features = [
     title: "No Red Tape",
     description:
       "Thrill cuts through the slow approval chains and corporate noise. You talk directly to the people who can say “yes” and make things happen, without endless forms or delays. The result — faster launches, smoother campaigns, and support that actually understands your goals.",
-    // svgPath: "/feature/feature5.svg",
-    svgComponent: Feature5SvgComponent ,
-    imagePath: "/feature/feature5.png",
+    svgComponent: Feature3SvgComponent ,
+    imagePath: "/feature/feature3.png",
     stats: {
       primary: {
         value: "Direct",
